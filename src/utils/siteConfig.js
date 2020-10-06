71a43ca9bbd4055a6afd115274e39d6b40f29cbe
vesaul <vesa.ulvi@gmail.com>
2020-10-06 23:09:30 +0300

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+    siteUrl: `xenodochial-montalcini-39437c.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
